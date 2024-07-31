c4d78fcad5446a95f2842949d9657fa6e9ca56dc
Benkhaoua Oussama <77804561+oussa29-dev@users.noreply.github.com>
2024-07-31 15:44:38 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const cors = require('cors');
 
 // express app
 const app = express()
 
 // middleware
+
+app.use(cors({
+  origin: 'https://workout-buddy-1-yze5.onrender.com'
+}));
+
 app.use(express.json())
 
 app.use((req, res, next) => {
