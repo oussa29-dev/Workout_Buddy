a6fff7cdba4f808e5df3ab791c0d17e85ce0b6c8
Benkhaoua Oussama <77804561+oussa29-dev@users.noreply.github.com>
2024-07-31 15:26:03 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
-
+const cors = require('cors');
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
-
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
