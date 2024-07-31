c2cb6cf588ff5505163690287e33e6d09d75d399
Benkhaoua Oussama <77804561+oussa29-dev@users.noreply.github.com>
2024-07-31 15:19:18 +0100

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('/api/user/signup', {
+    const response = await fetch('https://workout-buddy-i7tb.onrender.com/api/user/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
