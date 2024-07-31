a9f0e7b17154036ca44398db46b4de69de71083b
Benkhaoua Oussama <77804561+oussa29-dev@users.noreply.github.com>
2024-07-31 14:01:36 +0100

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://workout-buddy-i7tb.onrender.com/api/workouts', {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
