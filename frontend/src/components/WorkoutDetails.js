f1a6847166a6973c005b7d733fae04bd92271881
Benkhaoua Oussama <77804561+oussa29-dev@users.noreply.github.com>
2024-11-09 17:17:35 +0100

    Update WorkoutDetails.js

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -29,8 +29,8 @@ const WorkoutDetails = ({ workout }) => {
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p><strong>Price (DA): </strong>{workout.load}</p>
+      <p><strong>Months of Installment: </strong>{workout.reps}</p>
       <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
     </div>
